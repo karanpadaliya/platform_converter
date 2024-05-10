c1707722b8a9648872c5e8a4fb2cf700d86addc2
cpp_project1 <kdpadaliya216@gmail.com>
2024-05-10 10:16:01 +0530

    commit

--- lib/controller/IosProvider.dart
+++ lib/controller/IosProvider.dart
@@ -12,7 +12,36 @@ class IosProvider extends ChangeNotifier {
   }
 
   void changeTheme() {
-   Theme_Mode =! Theme_Mode;
+    Theme_Mode = !Theme_Mode;
     notifyListeners();
+  }
+
+//   Image Set
+  String? XFile;
+
+  void setImage(getImage) {
+    XFile = getImage;
+  }
+
+// Method to clear the image-related data
+  void clearImage() {
+    XFile = null; // Clear the image file path
+    isImage = false; // Update the flag indicating whether an image is present
+    notifyListeners();
+  }
+
+//   Profile Name, Bio
+  String? ProfileName;
+  String? ProfileBio;
+
+  void setProfile(String Name, String Bio) {
+    ProfileName = Name;
+    ProfileBio = Bio;
+  }
+
+  bool isImage = false;
+
+  void removeImg(img) {
+    isImage = img;
   }
 }
--- lib/view/HomePage/IosScreen/SettingsPage.dart
+++ lib/view/HomePage/IosScreen/SettingsPage.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:platform_converter/controller/IosProvider.dart';
 import 'package:provider/provider.dart';
 
@@ -11,6 +13,9 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController ProfileNameController = TextEditingController();
+  TextEditingController ProfileBioController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -36,6 +41,7 @@ class _SettingsPageState extends State<SettingsPage> {
                       header: Text("Set Platform".toUpperCase()),
                       children: [
                         CupertinoListTile(
+                          padding: EdgeInsets.all(15),
                           leading: Icon(CupertinoIcons.person),
                           title: Text("Profile"),
                           subtitle: Text("Update Profile Data"),
@@ -53,55 +59,201 @@ class _SettingsPageState extends State<SettingsPage> {
                                   false,
                           child: Container(
                             height: 300,
-                            color: CupertinoColors.white,
+                            // color: CupertinoColors.white,
                             child: Column(
                               children: [
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Container(
-                                  height: 100,
-                                  width: 100,
-                                  decoration: BoxDecoration(
-                                    color: CupertinoColors.activeBlue,
-                                    borderRadius: BorderRadius.circular(100),
-                                  ),
-                                  child: IconButton(
-                                    onPressed: () {
+                                CupertinoButton(
+                                  onPressed: () {
+                                    // Show dialog to choose between camera or gallery
+                                    showCupertinoDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return CupertinoAlertDialog(
+                                          title: Text(
+                                            "Choose Image Source",
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                          content: SingleChildScrollView(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: ListBody(
+                                                children: [
+                                                  GestureDetector(
+                                                    onTap: () async {
+                                                      XFile? file =
+                                                          await ImagePicker()
+                                                              .pickImage(
+                                                                  source:
+                                                                      ImageSource
+                                                                          .camera);
 
-                                    },
-                                    icon: Icon(
-                                      CupertinoIcons.camera,
-                                      color: Colors.white,
-                                    ),
+                                                      if (file != null) {
+                                                        // Do something with the captured image file
+                                                        // For example, you can set it to the provider
+                                                        Provider.of<IosProvider>(
+                                                                context,
+                                                                listen: false)
+                                                            .setImage(
+                                                                file.path);
+                                                        Navigator.pop(context);
+                                                      } else {
+                                                        // Handle if the user cancels capturing the image
+                                                        print(
+                                                            'User cancelled capturing image Camera');
+                                                      }
+                                                    },
+                                                    child: Text(
+                                                      "Camera",
+                                                      style: TextStyle(
+                                                          fontSize: 17),
+                                                    ),
+                                                  ),
+                                                  Divider(),
+                                                  GestureDetector(
+                                                    onTap: () async {
+                                                      //   Handle camera option
+                                                      XFile? file =
+                                                          await ImagePicker()
+                                                              .pickImage(
+                                                                  source: ImageSource
+                                                                      .gallery);
+                                                      if (file != null) {
+                                                        // Do something with the captured image file
+                                                        // For example, you can set it to the provider
+                                                        Provider.of<IosProvider>(
+                                                                context,
+                                                                listen: false)
+                                                            .setImage(
+                                                                file.path);
+                                                        Navigator.pop(context);
+                                                      } else {
+                                                        print(
+                                                            'User cancelled capturing image Gallery');
+                                                      }
+                                                    },
+                                                    child: Text(
+                                                      "Gallery",
+                                                      style: TextStyle(
+                                                          fontSize: 17),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: CircleAvatar(
+                                    radius: 50,
+                                    backgroundColor: Provider.of<IosProvider>(context).isImage
+                                        ? CupertinoColors.activeBlue
+                                        : null,
+                                    backgroundImage: Provider.of<IosProvider>(context).isImage
+                                        ? null
+                                        : FileImage(
+                                        File(value.XFile ?? "Image_NOT_Found")),
+                                    child: CupertinoButton(
+                                        child: Icon(
+                                          CupertinoIcons.camera,
+                                          // color: Colors.white,
+                                        ),
+                                        onPressed: () {}),
                                   ),
                                 ),
                                 Padding(
-                                  padding: const EdgeInsets.only(top: 10,left: 30,right: 30,bottom: 15),
+                                  padding: const EdgeInsets.only(
+                                      top: 10, left: 30, right: 30, bottom: 15),
                                   child: CupertinoTextField(
+                                    controller: ProfileNameController,
                                     decoration: BoxDecoration(
-                                      color: CupertinoColors.lightBackgroundGray.withOpacity(0.5),
+                                      color: CupertinoColors.lightBackgroundGray
+                                          .withOpacity(0.5),
                                       borderRadius: BorderRadius.circular(10),
                                     ),
                                     placeholder: "Name",
                                   ),
                                 ),
                                 Padding(
-                                  padding: const EdgeInsets.only(left: 30,right: 30,bottom: 20),
+                                  padding: const EdgeInsets.only(
+                                      left: 30, right: 30, bottom: 20),
                                   child: CupertinoTextField(
+                                    controller: ProfileBioController,
                                     decoration: BoxDecoration(
-                                      color: CupertinoColors.lightBackgroundGray.withOpacity(0.5),
+                                      color: CupertinoColors.lightBackgroundGray
+                                          .withOpacity(0.5),
                                       borderRadius: BorderRadius.circular(10),
                                     ),
                                     placeholder: "Bio",
                                   ),
                                 ),
-                                TextButton(onPressed: (){}, child: Text("Save"))
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  // mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        ProfileNameController.clear();
+                                        ProfileBioController.clear();
+                                        Provider.of<IosProvider>(context, listen: false)
+                                            .clearImage();
+                                      },
+                                      child: Text(
+                                        "Reset",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        // String? ProfileName = ProfileNameController.text;
+                                        if (ProfileNameController
+                                                .text.isNotEmpty &&
+                                            ProfileBioController
+                                                .text.isNotEmpty) {
+                                          Provider.of<IosProvider>(context,
+                                                  listen: false)
+                                              .setProfile(
+                                                  ProfileNameController.text,
+                                                  ProfileBioController.text);
+
+                                          showCupertinoModalPopup(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                return CupertinoActionSheet(
+                                                  title: Text("Saved!!!"),
+                                                  cancelButton:
+                                                      CupertinoActionSheetAction(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Text("Ok"),
+                                                  ),
+                                                );
+                                              });
+                                          print(
+                                              "Name= ${ProfileNameController.text} AND Bio= ${ProfileBioController.text}");
+                                        } else {
+                                          print("Field is Blank");
+                                        }
+                                      },
+                                      child: Text(
+                                        "Save",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                           ),
                         ),
                         CupertinoListTile(
+                          padding: EdgeInsets.all(15),
                           leading: Icon(CupertinoIcons.brightness),
                           title: Text("Theme"),
                           subtitle: Text("Change Theme"),
